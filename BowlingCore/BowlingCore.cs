6610299a79ff9cfce8ac5c73b4deb7bd55214ec7
Vitoria Reis <72982321+Vitoria-Reis@users.noreply.github.com>
2020-10-30 13:40:52 -0300

    feat: Logic and programming of cs and tests

--- BowlingCore/BowlingCore.cs
+++ BowlingCore/BowlingCore.cs
@@ -2,7 +2,38 @@
 
 namespace BowlingCore
 {
-    public class BowlingCore
+    public class BowlingScore
     {
+        private int[] rolls = new int[21];
+        private int JogadaAtual = 0;
+        
+       public void roll(int pins)
+       {
+           rolls[this.JogadaAtual++] = pins;
+       }
+        public double Score()
+        {
+            int score = 0;
+            int Frames = 0;
+            for(int Frame = 0; Frame < 10; Frame++)
+            {
+                    if(this.rolls[Frames] == 10)
+                    {
+                        score += 10 + this.rolls[Frames +1] + this.rolls[Frames +2];
+                        Frames ++;
+                    }
+                    else if(this.rolls[Frames] + this.rolls[Frames + 1] == 10)
+                    {
+                        score += 10 + this.rolls[Frames +2];
+                        Frames += 2;
+                    }
+                    else
+                    {
+                        score += this.rolls[Frames] + this.rolls[Frames + 1];
+                        Frames += 2;
+                    }
+            }
+            return score;
+        }
     }
 }
